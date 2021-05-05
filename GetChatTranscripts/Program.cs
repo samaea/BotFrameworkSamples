3c9e536da71fef4c74aa274699f1ea708750d7ca
Sam EA <sam.elanis@microsoft.com>
2021-05-05 17:55:35 +0100

    Initial commit of GetChatTranscripts

---
+++ GetChatTranscripts/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure.Blobs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GetChatTranscripts
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var conversationId = "";
+            var channelId = "directline";
+            ITranscriptStore store = new BlobsTranscriptStore(ConfigurationManager.AppSettings["AzureStorageConnectionString"], ConfigurationManager.AppSettings["AzureBlobStorageContainer"]);
+            var transcripts = await store.GetTranscriptActivitiesAsync(channelId,
+conversationId);
+            Transcript transcript = new Transcript(transcripts.Items.ToList().ConvertAll(o => (Activity)o));
+
+            // Displaying list of activities into a string
+
+            foreach (var transcriptEntity in transcript.Activities)
+            {
+                Console.WriteLine("From:- " + transcriptEntity.From.Name + " Message:- " + transcriptEntity.Text);  
+            }
+
+            Console.ReadKey();
+        }
+        
+
+    }
+}
---
+++ GetChatTranscripts/README.md
@@ -0,0 +1,3 @@
+# Retrieve Bot Framework Transcript files sample
+
+This sample demonstrates how you can retrieve the transcript files from Azure Blob Storage that were generated from the Transcription Middleware.
